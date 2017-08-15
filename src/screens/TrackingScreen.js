f3f7a23d4abf20e6f44054f40fe665d12dc082f2
Cooper Brown <cooperbrown9e@gmail.com>
2017-08-14 17:54:27 -0700

    added polylines to map

--- src/screens/TrackingScreen.js
+++ src/screens/TrackingScreen.js
@@ -8,18 +8,35 @@ import { View,
 } from 'react-native';
 import { MapView } from 'expo';
 import NavBar from '../ui-elements/nav-bar.js';
+import {Course} from '../../assets/bloomsday-path.js';
 
 
-export default class App extends React.Component {
+
+export default class TrackingScreen extends React.Component {
+
+
 
   static navigationOptions = {
     header: null,
   };
 
+  componentDidMount() {
+
+  }
+
+  componentWillMount() {
+  }
+
   dropDownMenu(){
     console.log("Drop Down Accessed");
   }
 
+  getCoords(lat, lon) {
+    let coords = [ {latitude: Number, longitude: Number} ];
+    BloomsdayCourse.map(c => {
+      coords.push({ latidude: c.Latidude, longitude: c.Longitude});
+    });
+  }
 
   render() {
     return (
@@ -33,6 +50,9 @@ export default class App extends React.Component {
             longitudeDelta: 0.0421,
           }}
         >
+
+        <MapView.Polyline coordinates={Course} strokeWidth={1} />
+
           <NavBar leftButton={<Image source={require('../../assets/icons/bars.png')} style={{height: 20, width: 20, tintColor: 'white'}}/>}
                   rightButton={<Image source={require('../../assets/icons/profile.png')} style={{height: 22, width: 22, tintColor: 'white'}}/>}
                   title={<TouchableOpacity onPress={this.dropDownMenu.bind(this)}>
