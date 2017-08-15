bd9db4e3de463e098e1c27a9096e2b3c3392cd78
Cooper Brown <cooperbrown9e@gmail.com>
2017-08-14 18:10:34 -0700

    edits

--- src/screens/TrackingScreen.js
+++ src/screens/TrackingScreen.js
@@ -8,6 +8,7 @@ import { View,
 } from 'react-native';
 import { MapView } from 'expo';
 import NavBar from '../ui-elements/nav-bar.js';
+import { Course } from '../../assets/bloomsday-path.js';
 
 
 export default class App extends React.Component {
@@ -27,12 +28,13 @@ export default class App extends React.Component {
         <MapView
           style={styles.map}
           initialRegion={{
-            latitude: 51.38254,
-            longitude: -2.362804,
+            latitude: 47.38254,
+            longitude: -117.362804,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
         >
+        <MapView.Polyline coordinates={Course} strokeWidth={1} />
           <NavBar leftButton={<Image source={require('../../assets/icons/bars.png')} style={{height: 20, width: 20, tintColor: 'white'}}/>}
                   rightButton={<Image source={require('../../assets/icons/profile.png')} style={{height: 22, width: 22, tintColor: 'white'}}/>}
                   title={<TouchableOpacity onPress={this.dropDownMenu.bind(this)}>
