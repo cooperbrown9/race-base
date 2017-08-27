fb4106c45e5bf146d0af1d0c973ff75e9e961abc
Cooper Brown <cooperbrown9e@gmail.com>
2017-08-27 13:43:49 -0700

    merge conflicts

--- src/reducers/navigation-reducer.js
+++ src/reducers/navigation-reducer.js
@@ -3,7 +3,7 @@ import { NavigationActions } from 'react-navigation';
 import * as NavActions from '../action-types/navigation-action-types.js';
 import { AppNavigator } from '../navigation/navigator.js';
 
-const firstAction = AppNavigator.router.getActionForPathAndParams('HistoricalWeather');
+const firstAction = AppNavigator.router.getActionForPathAndParams('Home');
 const firstState = AppNavigator.router.getStateForAction(firstAction);
 
 export default function nav(state = firstState, action) {
