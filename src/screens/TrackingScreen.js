46ffa0bc1c367740496ffb038757409d939adf2b
Cooper Brown <cooperbrown9e@gmail.com>
2017-10-31 17:24:47 -0700

    adding functionality for tracking users phone

--- src/screens/TrackingScreen.js
+++ src/screens/TrackingScreen.js
@@ -11,6 +11,7 @@ import NavBar from '../ui-elements/nav-bar.js';
 import { courseCoords } from '../../assets/bloomsday-path.js';
 import { connect } from 'react-redux';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Constants, Location, Permissions } from 'expo';
 import Menu from './Menu.js';
 import SideMenu from 'react-native-side-menu';
 import Timer from '../ui-elements/timer.js';
@@ -31,16 +32,50 @@ class TrackingScreen extends React.Component {
       pace: 0
     },
     coordCounter: 0,
-    currentLocation: { lat: 0, lng: 0 }
+    currentLocation: { lat: 0, lng: 0 },
+    userLocation: {},
+    userCoords: []
   }
 
   componentDidMount() {
     this.setCoordinates();
+    this.getLocationAsync();
 
-    setInterval(() => {
-      this.trackRunner();
-      this.handleAddLine({lat: this.state.coordinates[this.state.coordCounter].lat, lng:this.state.coordinates[this.state.coordCounter].lng });
-    }, 3000);
+    // setInterval(() => {
+    //   // this.trackRunner();
+    //   // this.handleAddLine(this.state.userLocation);
+    //   // this.handleAddLine({lat: this.state.coordinates[this.state.coordCounter].lat, lng:this.state.coordinates[this.state.coordCounter].lng });
+    // }, 3000);
+
+    // get user location
+    setInterval(async() => {
+      let { coords } = await Location.getCurrentPositionAsync({});
+      this.setState({ userLocation: { lat: coords.latitude, lng: coords.longitude }});
+
+      // looking for error in GPS, if location coords are the same as the previous coords, ignore them, otherwise add them
+      if (this.state.userCoords[this.state.userCoords.length - 1].lat != this.state.userLocation.lat ||
+        this.state.userCoords[this.state.userCoords.length - 1].lng != this.state.userLocation.lng) {
+          this.setState({ userCoords: [...this.state.userCoords, { lat: this.state.userLocation.lat, lng: this.state.userLocation.lng }]});
+      }
+      console.log(this.state.userLocation);
+
+
+      this.handleAddLineCoop();
+      this.trackRunnerCoop();
+    }, 5000);
+  }
+
+  getLocationAsync = async() => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if(status !== 'granted') {
+      this.setState({ canAccessLocation: false });
+    } else {
+      this.setState({ canAccessLocation: true });
+
+      let location = await Location.getCurrentPositionAsync({});
+      this.setState({ userLocation: { lat: location.coords.latitude, lng: location.coords.longitude }, userCoords: [...this.state.userCoords,  { lat: location.coords.latitude, lng: location.coords.longitude} ] });
+    }
   }
 
   toggleMenu = () => {
@@ -76,17 +111,117 @@ class TrackingScreen extends React.Component {
       }
   }
 
+  // PRE: added newest coords to userCoords
+  trackRunnerCoop = () => {
+    if(this.state.userCoords.length > 1) {
+        const lat1 = this.state.userCoords[this.state.userCoords.length - 2].lat;
+        const lon1 = this.state.userCoords[this.state.userCoords.length - 2].lng;
+        const lat2 = this.state.userCoords[this.state.userCoords.length - 1].lat;
+        const lon2 = this.state.userCoords[this.state.userCoords.length - 1].lng;
+        
+        function deg2rad(deg) {
+          return deg * (Math.PI/180)
+        }
+
+        var R = 3959; // Radius of the earth in miles
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+
+        var a =
+          Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in miles
+        this.setState({ runnerObj: { distance: d + this.state.runnerObj.distance } });
+        console.log(d);
+      }
+  }
+
+  handleAddLineCoop = (event) => {
+    // console.log('the longitude is ' + event.latLng.lng());
+    // this.setState({coordCounter: this.state.coordCounter+1, currentLocation: { lat: this.state.coordinates[this.state.coordCounter].lat, lng: this.state.coordinates[this.state.coordCounter].lng } });
+
+
+//     this.setState({
+//       coordinates: [
+//         {
+//           lat: event.lat,//latLng.lat(),
+//           lng: event.lng,//latLng.lng(),
+//         },
+//         ...this.state.coordinates,
+//       ]
+//     });  
+
+  if(this.state.userCoords.length > 1) {
+      console.log(this.state.userCoords);
+
+      const totalDistance = [];
+      const totalCoordinates = this.state.userCoords.length - 1;
+      const totalCoordinatesAdjusted = totalCoordinates - 1;
+
+      function getSum(total, num) {
+        return total + num;
+      }
+
+      for (var i = 0; i < totalCoordinates; i++) {
+        if (i != totalCoordinates) {
+          const lat1 = this.state.userCoords[i].lat;
+          const lon1 = this.state.userCoords[i].lng;
+          const lat2 = this.state.userCoords[i+1].lat;
+          const lon2 = this.state.userCoords[i+1].lng;
+
+          // this.setState({ currentLocation: {lat: lat1, lng: lon1}});
+
+          console.log(lat1);
+          console.log(lon1);
+
+          function deg2rad(deg) {
+            return deg * (Math.PI/180)
+          }
+
+          var R = 3959; // Radius of the earth in miles
+          var dLat = deg2rad(lat2-lat1);  // deg2rad below
+          var dLon = deg2rad(lon2-lon1);
+          var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+          var d = R * c; // Distance in miles
+          console.log('line segment ' + (i+1) + ' of ' + totalCoordinates + ' length is ' + d);
+          totalDistance.push(d);
+
+          const newTotal = totalDistance.reduce(getSum);
+          console.log('total distance is ' + newTotal);
+        }
+        if (i == totalCoordinates - 1) {
+          console.log('last one');
+          const newTotal = totalDistance.reduce(getSum).toFixed(2);
+
+          this.setState({
+            totalDistance: newTotal
+          });
+          console.log('total distance is now ' + this.state.totalDistance)
+        }
+      }
+    }  
+  }
+
   handleAddLine = (event) => {
     // console.log('the longitude is ' + event.latLng.lng());
     this.setState({coordCounter: this.state.coordCounter+1, currentLocation: { lat: this.state.coordinates[this.state.coordCounter].lat, lng: this.state.coordinates[this.state.coordCounter].lng } });
 
+
     this.setState({
       coordinates: [
-        ...this.state.coordinates,
         {
           lat: event.lat,//latLng.lat(),
           lng: event.lng,//latLng.lng(),
         },
+        ...this.state.coordinates,
       ]
     });  
 
@@ -171,6 +306,13 @@ class TrackingScreen extends React.Component {
 
   render() {
     const { width, height } = Dimensions.get('window');
+
+    let userCoords = this.state.userCoords;
+    let mappedUserCoords = [];
+    for(let i = 0; i < this.state.userCoords.length; i++) {
+      mappedUserCoords.push({ latitude: this.state.userCoords[i].lat, longitude: this.state.userCoords[i].lng });
+    }
+
     return (
       <View style={{backgroundColor:'transparent', flex: 1}}>
         <NavBar leftButton={<Image source={require('../../assets/icons/bars.png')} style={{height: 22, width: 22, tintColor: 'white'}} />}
@@ -188,6 +330,7 @@ class TrackingScreen extends React.Component {
             longitudeDelta: 0.0421,
         }} >
           <MapView.Polyline coordinates={courseCoords} strokeWidth={5} strokeColor={'#F4C81B'} />
+          <MapView.Polyline coordinates={mappedUserCoords} strokeWidth={5} strokeColor={'blue'} />
           <MapView.Marker coordinate={{latitude: 47.6588, longitude: -117.4260}} image={require('../../assets/icons/pin.png')} />
           <MapView.Marker coordinate={{latitude: this.state.currentLocation.lat, longitude: this.state.currentLocation.lng }} image={require('../../assets/icons/pin.png')} />
     </MapView>
