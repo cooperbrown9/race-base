2ec9b0344058eb9040e88c97015480cf2c291df1
Cooper Brown <cooperbrown9e@gmail.com>
2018-03-31 16:04:41 -0700

    e

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+// var OneSignal = require('react-native-onesignal').OneSignal;
 // import * as NavActions from './src/action-types/navigation-action-types';
 
 import { Permissions, Notifications } from 'expo';
@@ -12,6 +12,7 @@ import MainReducer from './src/reducers/main-reducer.js';
 import AppNavigatorWithState from './src/navigation/navigator.js';
 
 import { Font } from 'expo';
+import axios from 'axios';
 
 import thunk from 'redux-thunk';
 //change
@@ -38,44 +39,121 @@ class App extends React.Component {
       'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
       'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
     });
+    // this.register();
+    await this.registerForPushNotificationsAsync();
     this.setState({ fontLoaded: true });
     // await this.registerForPushNotifications();
   }
 
-  async registerForPushNotifications() {
-    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  registerForPushNotificationsAsync = async() => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
 
-  if (status !== 'granted') {
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    if (status !== 'granted') {
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
       return;
     }
+
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    let data = {
+      'token': token
+    }
+
+    // POST the token to your backend server from where you can retrieve it to send push notifications.
+
+    axios.post('https://racebaseapi.herokuapp.com/api/send-token', data).then((response) => {
+      let status = response.data;
+      console.log(status);
+    }).catch((e) => {
+      console.log(e);
+    });
+
+
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
+    // return fetch(PUSH_ENDPOINT, {
+    //   method: 'POST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     token: {
+    //       value: token,
+    //     },
+    //     user: {
+    //       username: 'Brent',
+    //     },
+    //   }),
+    // });
   }
-  const token = await Notifications.getExpoPushTokenAsync();
 
-  this.subscription = Notifications.addListener(this.handleNotification);
+  _handleNotification = (notification) => {
+    this.setState({notification: notification}, () => {
+      console.log(notification, 'yup');
+    });
+  };
 
-  this.setState({
-    token,
-  }, () => {
-    this.sendPushNotification();
-  });
+  register() {
+    OneSignal.checkPermissions((permissions) => {
+      console.log(permissions);
+    });
+
+    var permissions = {
+      alert: true,
+      badge: true,
+      sound: true
+    }
+
+    OneSignal.requestPermissions(permissions);
+    OneSignal.registerForPushNotifications();
   }
 
-  sendPushNotification(token = this.state.token, title = 'bruh', body = 'its liiiiiit') {
-  return fetch('https://exp.host/--/api/v2/push/send', {
-    body: JSON.stringify({
-      to: token,
-      title: title,
-      body: body,
-      data: { message: `${title} - ${body}` },
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'POST',
-  });
-}
+//   async registerForPushNotifications() {
+//     const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+//
+//   if (status !== 'granted') {
+//     const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+//     if (status !== 'granted') {
+//       return;
+//     }
+//   }
+//   const token = await Notifications.getExpoPushTokenAsync();
+//
+//   this.subscription = Notifications.addListener(this.handleNotification);
+//
+//   this.setState({
+//     token,
+//   }, () => {
+//     this.sendPushNotification();
+//   });
+//   }
+//
+//   sendPushNotification(token = this.state.token, title = 'bruh', body = 'its liiiiiit') {
+//   return fetch('https://exp.host/--/api/v2/push/send', {
+//     body: JSON.stringify({
+//       to: token,
+//       title: title,
+//       body: body,
+//       data: { message: `${title} - ${body}` },
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     method: 'POST',
+//   });
+// }
 
   // checkUser = async() => {
   //   let userID = await AsyncStorage.getItem('USER_ID');
