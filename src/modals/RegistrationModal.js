0322b5a6979932696383006e6bf87818886e006e
Cooper Brown <cooperbrown9e@gmail.com>
2018-06-22 12:05:52 -0700

    e

---
+++ src/modals/RegistrationModal.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, TextInput, ScrollView, StyleSheet } from 'react-native';
+
+class RegistrationModal extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: ''
+    }
+  }
+
+  static propTypes = {
+    dismiss: PropTypes.func
+  }
+
+  fieldFactory(placeholder, text, updateState, keyboard = 'default') {
+    return (
+      <View style={styles.fieldContainer} >
+        <TextInput
+          selectionColor={Colors.PURPLE} autoCorrect={false}
+          style={styles.field}
+          placeholder={placeholder}
+          onChangeText={(text) => updateState(text)}
+          value={text}
+          keyboardType={keyboard || 'default'}
+          returnKeyType={'done'}
+        />
+      </View>
+    )
+  }
+
+  render() {
+    return(
+
+      <View>
+
+      <View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  fieldContainer: {
+    marginLeft: 32, marginRight: 32, marginBottom: 32,
+    height: 64, justifyContent: 'center',
+    borderBottomColor: 'yellow', borderBottomWidth: 2
+  },
+  field: {
+    color: 'green', fontSize: 24, fontFamily: 'roboto-bold'
+  },
+})
+
+export default RegistrationModal;
